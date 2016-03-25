1ce1ad852697c70ec6525806c97b025101f748d1
eamoses <e.a.moses@gmail.com>
2016-03-25 16:47:32 -0700

    added objective button functionality, added button to html and styled the popup box in css

--- app.js
+++ app.js
@@ -2,5 +2,12 @@
 console.log("App.js is running!");
 
 $(document).on('ready', function() {
+// objective button functionality
+  $("button.objective").on("click", function(){
+    $("div.col-md-6.col-md-offset-3.popup.hidden").removeClass("hidden");//opens popup to explain objective
+  });
+  $("button.close").on("click", function(){
+    $("div.col-md-6.col-md-offset-3.popup").addClass("hidden"); //makes popup go hidden again so you can play the game
+  });
 
 });
