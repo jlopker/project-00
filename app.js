837622221ce6fb7cad27448a2da688552fc81454
eamoses <e.a.moses@gmail.com>
2016-03-26 21:20:42 -0700

    tried creating a winner text on the page instead of an alert

--- app.js
+++ app.js
@@ -1,31 +1,42 @@
-
 console.log("App.js is running!");
 
 $(document).on('ready', function() {
 //***PULL THE IDS FROM HTML AND MAKE A VARIABLE TO SET AND TO CALL TO THEIR X-AXIS LOCATION***/
   var $aLocation = $("#playerAvenir");
   var aLocation = 0;
+  var winAvenirLocation = 85;
   var $gLocation = $("#playerGaramond");
   var gLocation = -2;
-  var winLocation = 90;
+  var winGaramondLocation = 83;
+  var winner = 'none';
 //***GRABS SVG A OR G AND ATTRIBUTES X AXIS LOCATION TO ITS INDIVIDUAL PERCENTAGE ON THE STARTING LINE***/
 /***THE EXACT PERCENTAGE SET TO IS DEPENDENT ON THE SIZE OF THE SVG WE ARE ANIMATING SO THEY APPEAR EQUAL ON THE PAGE***/
+$aLocation.attr('x', aLocation + '%');
+$gLocation.attr('x', gLocation + '%');
 //***UNPOPULATED CONSTRUCTOR*/a IS 65 FOR KEYCODE AND G IS 71 FOR KEYCODE**//
 $('body').keydown(function(event) {
-  if(event.keyCode === 65) {
+  if(event.keyCode === 65 && winner === 'none'){
     $aLocation.attr('x', aLocation + '%');
     aLocation++;
+    if(aLocation === winAvenirLocation){
+      winner = 'Avenir';
+      
+      var congrats = document.createElement("image");
+      congrats.className = "avenirWins";
+      congrats.appendChild(document.createTextNode("AVENIR WINS!"));
+      raceTrack.appendChild(congrats);
+    }
     console.log(aLocation);
-  }else if(event.keyCode === 71){
+  }else if(event.keyCode === 71 && winner === 'none'){
     $gLocation.attr('x', gLocation + '%');
     gLocation++;
+    if(gLocation === winGaramondLocation){
+      winner = 'Garamond';
+      alert(winner +' Wins!');
   }
+  console.log(gLocation);
+}
 });
-//***THEORETICALLY: WILL ALERT WHEN THE WINNING PLAYER REACHES THE FINISH LINE***//
-    while (aLocation === winLocation || gLocation === winLocation){
-      alert('WINS!');
-    }
-
 //***OBJECTIVE BUTTON FUNCTIONALITY***//
   $("button.objective").on("click", function(){
     $("div.col-md-6.col-md-offset-3.popup.hidden").removeClass("hidden");//opens popup to explain objective
@@ -33,7 +44,6 @@ $('body').keydown(function(event) {
   $("button.close").on("click", function(){
     $("div.col-md-6.col-md-offset-3.popup").addClass("hidden"); //makes popup go hidden again so you can play the game
   });
-
   $("button.replay").on("click", function(){ //onclick telling the button, when clicked, to reload the page for a new game
     location.reload();
   });
